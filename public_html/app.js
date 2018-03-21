5899b4fe64b16fb415700637fe259dfdb832e6c1
Heiðar Jónsson <heidarj@hraunteigur5.com>
2018-03-21 01:29:56 +0000

    created user object, which tracks each connection's username, id and color. CSS overhaul, removed header and footer to use fixed position inputbox at bottom. Added socket events and base system for 'commands' and 'variables' which the client can use to interact with the server

--- public_html/app.js
+++ public_html/app.js
@@ -1,32 +1,75 @@
-var socket = io();
+var username = document.getElementById("username");
+var submitUsername = document.getElementById("submitUsername");
+var socket;
+
+username.onkeydown = function(e) {
+	if (e.keyCode == 13 && username.value) {
+		connectSocket();
+	}
+};
+submitUsername.onclick = function() {
+	if (username.value) {
+		connectSocket();
+	}
+};
 
 var chatconsole = document.getElementById("chatconsole");
-var messagebox = document.getElementById("messagebox");
+var inputbox = document.getElementById("inputbox");
 var messages = document.getElementById("messages");
 
 chatconsole.onclick = function() {
-	messagebox.focus();
+	inputbox.focus();
 };
 
 messages.onclick = function() {
 	return null;
 };
 
-messagebox.onkeydown = function(e) {
+inputbox.onkeydown = function(e) {
 	if (e.keyCode == 13) {
 		sendMessage();
 	}
 };
 
 function sendMessage() {
-	if (messagebox.value) {
-		socket.emit("message", messagebox.value);
-		messagebox.value = "";
+	if (inputbox.value) {
+		switch (inputbox.value[0]) {
+			case "$":
+				socket.emit("command", inputbox.value);
+				break;
+			default:
+				socket.emit("message", inputbox.value);
+				break;
+		}
+		inputbox.value = "";
 	}
 }
 
-socket.on("message", function(e) {
+function addMessage(user, message, color) {
 	var li = document.createElement("li");
-	li.innerHTML = e;
+	li.innerHTML = formatUsername(user, color) + " > " + message;
 	messages.appendChild(li);
-});
+	chatconsole.scrollTo(0, chatconsole.scrollHeight);
+}
+
+function connectSocket() {
+	socket = io(("http://localhost:3000/?username=" + username.value)); // eslint-disable-line
+	document.body.removeChild(document.getElementById("loginOverlay"));
+
+	socket.on("message", function(e) {
+		addMessage(e.user.username, e.message, e.user.color);
+	});
+
+	socket.on("newConnection", function(e) {
+		addMessage("server", (formatUsername(e.user.username, e.user.color) + " has connected"));
+	});
+
+	socket.on("command", function(e) {
+		addMessage("server", (formatUsername(e.user.username, e.user.color) + e.message));
+	});
+
+}
+
+var formatUsername = function(username, color) {
+	return "<i style='color: " + (color ? color : "#00FFFF") + ";'>" + username + "</i>";
+};
--- server.js
+++ server.js
@@ -5,12 +5,49 @@ var server = app.listen(3000);
 var socket = require("socket.io");
 var io = socket(server);
 
+function HSLstring(h, s, l) {
+	return "hsl(" + h + ", " + s + "%, " + l + "%)";
+}
+
+var User = function(id, username, color) {
+	this.id = id;
+	this.username = username;
+	this.color = color;
+};
+
 app.use("/", express.static("public_html"));
 
 io.on("connection", function(socket) {
-	socket.broadcast.emit("newConnection", socket.client.id);
-	console.log("New connection: " + socket.client.id);
-	socket.on("message", e => {
-		io.emit("message", e);
+	var user = new User(socket.id, socket.handshake.query.username, HSLstring((Math.floor(Math.random() * 6) * 60), 100, 85));
+
+	io.emit("newConnection", {
+		"user": user
+	});
+	console.log("New connection: " + user.color);
+	socket.on("message", function(e) {
+		io.emit("message", {
+			"user": user,
+			"message": e
+		});
+	});
+	socket.on("command", function(e) {
+		var split = e.split(" = ");
+
+		switch (split[0]) {
+			case "$myColor":
+				if (split[1]) {
+					user.color = split[1];
+					io.emit("command", {
+						"user": user,
+						"message": (" just set his color to: " + user.color + " using the $myColor variable")
+					});
+				} else {
+					io.emit("command", {
+						"user": user,
+						"message": (" color is currently: " + user.color)
+					});
+				}
+				break;
+		}
 	});
 });
