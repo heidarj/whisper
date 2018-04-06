60145d77e9b2a1e7e53c431440cd04894ab0628e
Heiðar Jónsson <heidarj@hraunteigur5.com>
2018-04-06 23:28:31 +0000

    wow such commit, much code

---
+++ sequelize_models/user.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+	return sequelize.define("user", {
+		id: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		username: {
+			type: DataTypes.STRING,
+			unique: true,
+			allowNull: false
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [8, 200]
+			}
+		},
+		email: {
+			type: DataTypes.STRING,
+			unique: true
+		},
+		avatar: DataTypes.STRING,
+		token: DataTypes.STRING,
+		admin: DataTypes.BOOLEAN
+	});
+};
